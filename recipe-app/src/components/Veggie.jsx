a0a74bec29be024105315888bafd6931428f385e
Shirendev <shirendev.07@gmail.com>
2022-10-23 21:44:18 +0800

    finish

--- recipe-app/src/components/Veggie.jsx
+++ recipe-app/src/components/Veggie.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import styled from "styled-components"
 import {Splide, SplideSlide} from "@splidejs/react-splide"
 import '@splidejs/react-splide/css';
+import { Link } from "react-router-dom"
 
 function Veggie() {
 
@@ -38,7 +39,7 @@ function Veggie() {
                         <h3>Our Vegetarian Picks</h3>
                         <Splide options={{
                             perPage:3,
-                            arrows: false,
+                            arrows: false, 
                             pagination: false,
                             drag: 'free',
                             gap:'5rem'
@@ -47,9 +48,11 @@ function Veggie() {
                             return (
                                 <SplideSlide key={recipe.id}>
                                 <Card>
+                                    <Link to={'/recipe/'+recipe.id}>
                                     <p>{recipe.title }</p>
                                     <img src={recipe.image} alt={recipe.title} />
                                     <Gradient/>
+                                    </Link>
                                 </Card>
                                 </SplideSlide>
                             )
--- recipe-app/src/pages/Home.jsx
+++ recipe-app/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Veggie from '../components/Veggie'
 import Popular from '../components/Popular'
+import { motion } from 'framer-motion'
 export default function Home() {
   return (
-    <div><Veggie/>
+    <motion.div animate={{opacity:1}}
+    initial={{opacity:0}}
+    exit={{ opacity:0}}
+    transition={{duration: 0.5}}><Veggie/>
     <Popular/>
-    </div>
+    </motion.div>
   )
 }
