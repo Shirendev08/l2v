08516e44c3de4796523470592b0e852141d2446b
Shirendev <shirendev.07@gmail.com>
2022-10-23 10:46:56 +0800

    first

---
+++ recipe-app/src/pages/Cuisine.jsx
@@ -0,0 +1,26 @@
+import React, {useEffect, useState} from 'react'
+import styled from 'styled-components'
+import {motion} from 'framer-motion'
+import {link,useParams} from "react-router-dom"
+function Cuisine() {
+  const [cuisine, setCuisine] = useState([]);
+  let params = useParams();
+    const getCuisine = async (name) =>{
+        const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_KEY}&cuisine=${name}`)
+        const recipes = await data.json();
+        setCuisine(recipes.results)
+
+    }
+    useEffect(()=>{
+      // getCuisine('italian')
+      console.log(params)
+    }, [])
+  return (
+
+    <div>
+
+    </div>
+  )
+}
+
+export default Cuisine
---
+++ recipe-app/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Veggie from '../components/Veggie'
+import Popular from '../components/Popular'
+export default function Home() {
+  return (
+    <div><Veggie/>
+    <Popular/>
+    </div>
+  )
+}
